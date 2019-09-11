b31e2086d04254166c8443c264378327d44134b6
Iris Sakura <sgjsakura@hotmail.com>
2019-09-11 19:38:22 +0800

    Update

---
+++ Pomelo.Mangos.Authentication/Pomelo.Mangos.Authentication.Core/MemoryStreamHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Pomelo.Mangos.Authentication
+{
+	/// <summary>
+	/// Provide helper method for <see cref="MemoryStream"/>. This class is static.
+	/// </summary>
+	public static class MemoryStreamHelper
+	{
+		/// <summary>
+		/// Write an entire byte sequence into the memory stream.
+		/// </summary>
+		/// <param name="stream">The memory stream.</param>
+		/// <param name="buffer">The buffer to be written.</param>
+		public static void Write(this MemoryStream stream, byte[] buffer) => stream.Write(buffer, 0, buffer.Length);
+	}
+}
---
+++ Pomelo.Mangos.Authentication/Pomelo.Mangos.Authentication/AccountVerificationInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace Pomelo.Mangos.Authentication
+{
+	/// <summary>
+	/// Account verification information used to authenticate with user accounts.
+	/// </summary>
+	public class AccountVerificationInfo
+	{
+		/// <summary>
+		/// The salt.
+		/// </summary>
+		public string Salt { get; set; }
+		/// <summary>
+		/// The verifier.
+		/// </summary>
+		public string Verifier { get; set; }
+	}
+}
